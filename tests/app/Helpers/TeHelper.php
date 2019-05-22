36905e2e78874c3ba78afb3acd76cd0b1ef46269
hardcoder1991 <hardcoder1991@gmail.com>
2019-05-22 17:29:27 +0530

    Changes made after first commit here

--- tests/app/Helpers/TeHelper.php
+++ tests/app/Helpers/TeHelper.php
@@ -42,25 +42,22 @@ class TeHelper
 
     public static function willExpireAt($due_time, $created_at)
     {
-        $due_time = Carbon::parse($due_time);
-        $created_at = Carbon::parse($created_at);
+        $due_time = Carbon::parse($due_time)->format('Y-m-d H:i:s');
+        $created_at = Carbon::parse($created_at)->format('Y-m-d H:i:s');
 
         $difference = $due_time->diffInHours($created_at);
 
+        if($difference <= 90)
+            $time = $due_time;
+        elseif ($difference <= 24) {
+            $time = $created_at->addMinutes(90);
+        } elseif ($difference > 24 && $difference <= 72) {
+            $time = $created_at->addHours(16);
+        } else {
+            $time = $due_time->subHours(48);
+        }
 
-        // if($difference <= 90)
-        //     $time = $due_time;
-        // elseif ($difference <= 24) {
-        //     $time = $created_at->addMinutes(90);
-        // } elseif ($difference > 24 && $difference <= 72) {
-        //     $time = $created_at->addHours(16);
-        // } else {
-        //     $time = $due_time->subHours(48);
-        // }
 
-
-        $totalDiff = $due_time->diffForHumans($created_at);
-        dd($totalDiff);
 
         return $totalDiff->format('Y-m-d H:i:s');
 
